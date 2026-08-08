7d8da97cfaf366ebe66f325056d37ae34f1b1675
isdarlinm <82850219+IsdarlinM@users.noreply.github.com>
2026-08-08 01:04:08 -0400

    Register ReproSec 0.5 research CLI

--- src/reprosec/cli_all.py
+++ src/reprosec/cli_all.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import sys
 
 from . import cli_commands_runtime as _runtime
+from . import cli_research_context as _research_context  # noqa: F401
 from .api_vnext import create_app as create_vnext_app
 from .cli import normalize_help_argv
 from .cli_vnext import app
